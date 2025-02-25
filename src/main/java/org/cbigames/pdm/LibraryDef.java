2dfcb536b8dfdd4d0962098bb6a267ad678385c2
jSdCool <jSdCoolgaming@gmail.com>
2025-02-24 20:13:49 -0500

    functional

--- src/main/java/org/cbigames/pdm/LibraryDef.java
+++ src/main/java/org/cbigames/pdm/LibraryDef.java
@@ -3,10 +3,10 @@ package org.cbigames.pdm;
 import java.util.ArrayList;
 
 public class LibraryDef {
-    private String libPackage;
-    private String libJar;
+    private String libPackage = null;
+    private String libJar = null;
 
-    private String libVersion;
+    private String libVersion = null;
 
     private ArrayList<String> libNatives = new ArrayList<>();
     private ArrayList<String>  libDependsJars = new ArrayList<>();
@@ -35,6 +35,46 @@ public class LibraryDef {
                 continue;
             }
         }
+
+        if(libJar == null){
+            throw new RuntimeException("No jar specified for lib on line "+start);
+        }
+        if(libPackage == null){
+            throw new RuntimeException("No package specified for lib "+libJar);
+        }
+        if(libVersion == null){
+            throw new RuntimeException("No version specified for "+libPackage+":"+libJar);
+        }
     }
 
+    @Override
+    public String toString() {
+        return "LibraryDef{" +
+                "libPackage='" + libPackage + '\'' +
+                ", libJar='" + libJar + '\'' +
+                ", libVersion='" + libVersion + '\'' +
+                ", libNatives=" + libNatives +
+                ", libDependsJars=" + libDependsJars +
+                '}';
+    }
+
+    public ArrayList<String> getLibDependsJars() {
+        return libDependsJars;
+    }
+
+    public ArrayList<String> getLibNatives() {
+        return libNatives;
+    }
+
+    public String getLibJar() {
+        return libJar;
+    }
+
+    public String getLibPackage() {
+        return libPackage;
+    }
+
+    public String getLibVersion() {
+        return libVersion;
+    }
 }
--- src/main/java/org/cbigames/pdm/Main.java
+++ src/main/java/org/cbigames/pdm/Main.java
@@ -1,11 +1,10 @@
 package org.cbigames.pdm;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class Main {
     public static void main(String[] args) {
@@ -67,12 +66,13 @@ public class Main {
             }
             //if the line is defining a lib
             if(setupLines[lineNumber].startsWith("lib")){
-                if(setupLines[lineNumber].charAt(3)=='{'){
+                if(setupLines[lineNumber].charAt(3)=='{' || setupLines[lineNumber].charAt(4)=='{'){
+                    lineNumber++;
                     //find how long the lib block is
                     int length = 0;
                     int numBlocks = 1;
                     while(true){
-                        if(length+length>= setupLines.length){
+                        if(lineNumber+length >= setupLines.length){
                             throw new RuntimeException("Reached end of file before end of lib block");
                         }
                         if(setupLines[lineNumber+length].charAt(0)=='}'){
@@ -86,13 +86,159 @@ public class Main {
                         }
                         length++;
                     }
-                    libraries.add(new LibraryDef(setupLines,lineNumber+1,length));
+                    libraries.add(new LibraryDef(setupLines,lineNumber,length));
                     lineNumber+=length;
                 }
 
             }
         }
 
+        String[] processingLibs = new File(sketchBookLocation+"/libraries").list();
+
+        //prepare for installation
+        for(LibraryDef lib: libraries){
+            //check to see if the specified lib already exists
+            boolean exsists = false;
+            if(processingLibs != null) {
+                for (String fn : processingLibs) {
+                    if (fn.equals(lib.getLibJar())) {
+                        exsists = true;
+                    }
+                }
+            }
+            //if the lib is already installed then check the version
+            boolean delete = false;
+            if(exsists){
+                String fileVersion = null;
+                try(FileInputStream version = new FileInputStream(sketchBookLocation+"/libraries/"+lib.getLibJar()+"/library/version.txt")){
+                    fileVersion = new String(version.readAllBytes());
+                    if(fileVersion.equals(lib.getLibVersion())){
+                        System.out.println("Found the correct version of "+lib.getLibJar()+", skipping");
+                        continue;
+                    }else{
+                        System.out.println("Found an existing version of "+lib.getLibJar()+", but the version is incorrect. installing correct version");
+                        delete = true;
+                    }
+                }catch (IOException i){
+                    System.out.println("Found a lib in processing with the same name as one specified in the config, but no version file was found. considering it to be wrong version and re installing");
+                    delete = true;
+                }
+
+            }
+            if(delete){
+                //delete the exsisting file in the location
+                System.out.println("Deleting previous version of: "+lib.getLibJar());
+                deleteFilesInFolder(sketchBookLocation+"/libraries/"+lib.getLibJar()+"/library");
+            }else{
+                boolean ignored = new File(sketchBookLocation+"/libraries/"+lib.getLibJar()+"/library/").mkdirs();
+            }
+
+            //we now have an empty folder, lets download the things
+            //find what repo the thing is in
+            String repo = repos.get(0);
+
+            String libRepoPath = repo+"/"+lib.getLibPackage().replaceAll("\\.","/")+"/"+lib.getLibJar()+"/"+lib.getLibVersion();
+            //download the main library jar
+            try {
+                System.out.println("Downloading "+lib.getLibJar());
+                DownloadFile.download(libRepoPath+"/"+lib.getLibJar()+"-"+lib.getLibVersion()+".jar",sketchBookLocation+"/libraries/"+lib.getLibJar()+"/library/"+lib.getLibJar()+".jar",null);
+            } catch (IOException e) {
+                throw new RuntimeException("Exception while downloading jar: "+lib.getLibJar(),e);
+            }
+            //download any native jars
+            for(int i=0;i<lib.getLibNatives().size();i++){
+                System.out.println("Downloading native lib for "+lib.getLibJar());
+                try {
+                    DownloadFile.download(libRepoPath+"/"+lib.getLibJar()+"-"+lib.getLibVersion()+"-"+lib.getLibNatives().get(i)+".jar",sketchBookLocation+"/libraries/"+lib.getLibJar()+"/library/native_lib"+i+".jar",null);
+                } catch (IOException e) {
+                    throw new RuntimeException("Exception while downloading Natives for "+lib.getLibJar(),e);
+                }
+            }
+            //unpack the downloaded natives
+            if(!lib.getLibNatives().isEmpty()) {
+                System.out.println("Unpacking native libs");
+            }
+            File destDir = new File(sketchBookLocation+"/libraries/"+lib.getLibJar()+"/library/");
+            for(int i=0;i<lib.getLibNatives().size();i++){
+                try {
+                    ZipInputStream zis = new ZipInputStream(new FileInputStream(sketchBookLocation+"/libraries/"+lib.getLibJar()+"/library/native_lib"+i+".jar"));
+                    ZipEntry ze = zis.getNextEntry();
+                    while (ze != null){
+                        File newFile = newFile(destDir,ze);
+                        if(newFile.isDirectory()){
+                            if(newFile.getName().equals("META-INF")){
+                                ze = zis.getNextEntry();
+                                continue;
+                            }
+                            if(!newFile.mkdirs()){
+                                throw new RuntimeException("Failed to create Folder "+newFile);
+                            }
+                        }else{
+                            File parent = newFile.getParentFile();
+                            if(parent.getPath().contains("META-INF")){
+                                ze = zis.getNextEntry();
+                                continue;
+                            }
+                            if(newFile.getName().equals("META-INF")){
+                                ze = zis.getNextEntry();
+                                continue;
+                            }
+                            if (!parent.isDirectory() && !parent.mkdirs()) {
+                                throw new IOException("Failed to create directory " + parent);
+                            }
+                            try(FileOutputStream fos = new FileOutputStream(newFile);){
+                                zis.transferTo(fos);//lets see if this works
+                            }
+                        }
+                        ze = zis.getNextEntry();
+                    }
+                    zis.closeEntry();
+                    zis.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            //finally wright the version file
+            try (PrintWriter pr = new PrintWriter(sketchBookLocation + "/libraries/" + lib.getLibJar() + "/library/version.txt")) {
+                pr.write(lib.getLibVersion());
+                pr.flush();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+
+
+    }
+
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+
+    static void deleteFilesInFolder(String folder){
+        String[] files = new File(folder).list();
+        if(files == null){
+            return;
+        }
+        for(String thing: files){
+            File file = new File(folder+"/"+thing);
+            if(file.isDirectory()){
+                deleteFilesInFolder(folder+"/"+thing);
+            }
+            boolean ignored = file.delete();
+        }
     }
 
     public enum OS{
