5379ff71c762ddb79b21ebba84cbb851f19c1891
jSdCool <jSdCoolgaming@gmail.com>
2026-05-26 19:13:28 -0400

    fix file download not following redirects

--- src/main/java/org/cbigames/pdm/DownloadFile.java
+++ src/main/java/org/cbigames/pdm/DownloadFile.java
@@ -5,9 +5,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 public class DownloadFile {
   static void download(String link, String fileName,long[] progress) throws IOException {
@@ -15,9 +18,18 @@ public class DownloadFile {
     URL url = new URL(link);
     URLConnection c = url.openConnection();
     c.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.0.3705; .NET CLR 1.1.4322; .NET CLR 1.2.30703)");
+    Map<String, List<String>> headers = c.getHeaderFields();//follow link redirects
+    for (String header : headers.get(null)) {
+      if (header.contains(" 302 ") || header.contains(" 301 ")) {
+        link = headers.get("Location").get(0);
+        url = new URL(link);
+        c = url.openConnection();
+        headers = c.getHeaderFields();
+      }
+    }
     if(progress!=null && progress.length>=2){
       progress[0] = c.getContentLengthLong();
-    }
+    }System.err.println(c.getContentLengthLong());
 
     InputStream input;
     input = c.getInputStream();
@@ -33,6 +45,7 @@ public class DownloadFile {
         }
       }
     }
+    input.close();
     output.close();
   }
 
@@ -52,6 +65,7 @@ public class DownloadFile {
         output.append(new String(buffer,0,n));
       }
     }
+    input.close();
     return output.toString();
   }
 }
